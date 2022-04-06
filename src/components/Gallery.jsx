d6b3268717d2aafdeb6ebc26467f302d55881411
afsumeem <afsumeem@gmail.com>
2022-04-06 11:24:20 +0600

    gallery added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,31 @@
 
 function Gallery() {
     return (
-        <section className="p-10">
+        <section className="py-10">
             {/* section title */}
             <div>
-                <h1
-                    className="text-7xl text-red-500 text-center font-medium tracking-tighter pb-[20px] px-[20px] section-title"
+                <h1 className="text-7xl text-red-500 text-center font-medium tracking-tighter pb-[20px] px-[20px] section-title"
                 >
                     Our Gallery
                 </h1>
-                <h2
-                    className="text-center text-[#84795D] text-2xl section-para mb-5 font-semibold leading-7"
+                <h2 className="text-center text-[#84795D] text-2xl section-para mb-5 font-semibold leading-7"
                 >
                     An event that gives back
                 </h2>
             </div>
-            <div>
+            <div className="bg-white grid md:grid-cols-3 gap-8 px-11 pt-11 pb-3.5 mx-2.5">
+                {/* gallery column 1 */}
+                <div>
 
+                </div>
+                {/* gallery column 2 */}
+                <div>
+
+                </div>
+                {/* gallery column 3 */}
+                <div>
+
+                </div>
             </div>
         </section>
     );
