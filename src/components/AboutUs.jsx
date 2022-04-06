707604fc5a5a9bfc21b3edd6ee3b62e9966f6f23
afsumeem <afsumeem@gmail.com>
2022-04-06 12:16:38 +0600

    gallery done

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -22,7 +22,7 @@ function AboutUs() {
           We might get listed as Wedding Planner or Event Manager but we are above these mediocre terms. We’re Counselors. We do all what others do but in a different way. We simply instill life in your celebration and you will say, Superb Celebration it is. We’re a bunch of people who congregated on a same motto to create spectacular events. We are self-driven individuals who work as a gelled team. We create ideas out of nowhere and then brainstorm. From the main entrance design to the corner which will be least visible, we work on each and every part with a thorough plan.
         </p>
         <button
-          className="bg-red-500 hover:bg-red-700 hover:duration-300 hover:-translate-y-2 text-xs font-bold uppercase leading-7 tracking-[2.5px] text-white px-5 py-1.5 text-center block m-auto rounded-sm "
+          className="bg-red-500 hover:bg-red-700 hover:duration-300 hover:-translate-y-2 text-xs font-bold uppercase leading-7 tracking-[2.5px] text-white px-5 py-1.5 text-center block m-auto rounded-sm"
         >Know Us Better</button>
       </div>
     </section>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,13 @@
+import galleryImg1 from "../assets/images/gallery-1.png";
+import galleryImg2 from "../assets/images/gallery-1.1.png";
+import galleryImg3 from "../assets/images/gallery-1.2.png";
+import galleryImg4 from "../assets/images/gallery-2.jpg";
+import galleryImg5 from "../assets/images/gallery-2.1.jpg";
+import galleryImg6 from "../assets/images/gallery-2.2.jpg";
+import galleryImg7 from "../assets/images/gallery-3.jpeg";
+import galleryImg8 from "../assets/images/gallery-3.1.jpeg";
+import galleryImg9 from "../assets/images/gallery-3.3.jpeg";
+
 
 function Gallery() {
     return (
@@ -13,20 +23,65 @@ function Gallery() {
                     An event that gives back
                 </h2>
             </div>
-            <div className="bg-white grid md:grid-cols-3 gap-8 px-11 pt-11 pb-3.5 mx-2.5">
+            <div className="bg-white grid md:grid-cols-3 gap-5 px-20 pt-5 pb-3.5 mx-2.5">
                 {/* gallery column 1 */}
                 <div>
-
+                    <div className='galleryImg drop-shadow-2xl'>
+                        <img src={galleryImg1} alt="galleryImg" />
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-4 pt-4">
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg2} alt="galleryImg" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg3} alt="galleryImg" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 {/* gallery column 2 */}
                 <div>
-
+                    <div className='galleryImg drop-shadow-2xl'>
+                        <img src={galleryImg4} alt="galleryImg" />
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-4 pt-4">
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg5} alt="galleryImg" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg6} alt="galleryImg" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 {/* gallery column 3 */}
                 <div>
-
+                    <div className='galleryImg drop-shadow-2xl'>
+                        <img src={galleryImg7} alt="galleryImg" />
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-4 pt-4">
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg8} alt="galleryImg" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className='galleryImg drop-shadow-2xl'>
+                                <img src={galleryImg9} alt="galleryImg" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <button
+                className="bg-red-500 hover:bg-red-700 hover:duration-300 hover:-translate-y-2 text-xs font-bold uppercase leading-7 tracking-[2.5px] text-white px-5 py-1.5 mt-4 text-center block m-auto rounded-sm"
+            >Explore More</button>
         </section>
     );
 }
